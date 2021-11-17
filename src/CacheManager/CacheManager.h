9745d71a86bee10b6cd678898592ef8e59ce7a66
Lydia <ccjs@bu.edu>
2021-11-17 12:43:55 -0500

    updated the test so it can be used to try an arbitrary thread on an arbitraty board

--- src/CacheManager/CacheManager.h
+++ src/CacheManager/CacheManager.h
@@ -9,7 +9,7 @@
 #include <sstream>
 #include <sys/stat.h>
 
-#define REFRESH_COOLDOWN 100
+#define REFRESH_COOLDOWN 1
 #define FILEEXISTS(_path_) (access(_path_.c_str(), F_OK) != -1)
 
 using namespace std;
--- src/test.cpp
+++ src/test.cpp
@@ -42,13 +42,37 @@ testOriginalPost(string board, unsigned int thread_no)
 }
 
 int
+getThreadNo(string board, unsigned int page, unsigned int thread)
+{
+    int thread_no;
+    ifstream thread_file;
+    CacheManager CM;
+    const string home = getenv("HOME");
+    string file_path = home + "/" 
+            + ".final_project/cache/"
+            + board
+            +"/threads.json";
+    CM.prepCache(board,0);
+    Json::Value thread_info;
+    thread_file.open(file_path.c_str(), std::ios::binary);
+    thread_file >> thread_info;
+    thread_file.close();
+    thread_no = thread_info[page]["threads"][thread]["no"].asInt();
+    cout << thread_no << endl;
+    return thread_no;
+
+}
+
+int
 main(int argc, char **argv)
 {
     if(argc > 1)
     {
         string board = string(argv[1]);
-        unsigned int thread_no = atoi(argv[2]);
-        testCacheManager(board);
+        int page = atoi(argv[2]);
+        int thread = atoi(argv[3]);
+        unsigned int thread_no;
+        thread_no = getThreadNo(board, page,thread);
         testOriginalPost(board,thread_no);
     }
     else
